b19d6bc3228523a6205e1488392a0cccc476760f
chrishoen <chris123hoen@gmail.com>
2019-11-05 22:15:58 -0500

    more

--- build2.sh
+++ build2.sh
@@ -2,6 +2,6 @@
 
 echo "Compiling the overlay from .dts to .dtbo"
 
-dtc -O dtb -o My-UART4-RS485-00A0.dtbo -b 0 -@ My-UART4-RS485.dts
+dtc -O dtb -o MyUART4-00A0.dtbo -b 0 -@ MyUART4.dts
 
 sudo cp My-UART4-RS485-00A0.dtbo /lib/firmware
